df39a1ecc7c83371b5ed2af24b440cb1d2b33f8d
Yinghan <evelynlong95@homail.com>
2021-10-19 16:00:41 -0400

    first commit

---
+++ Research/run_model.py
@@ -0,0 +1,69 @@
+from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
+from datasets import load_dataset
+from datasets import load_metric
+from transformers import AutoTokenizer
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2Model.from_pretrained('gpt2')
+dataset = load_dataset("cnn_dailymail")
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+full_train_dataset = tokenized_datasets["train"]
+full_eval_dataset = tokenized_datasets["test"]
+
+#Use rouge metric
+metric = load_metric("rouge")
+
+#finetuning with trainer
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=3,              # total number of training epochs
+    per_device_train_batch_size=16,  # batch size per device during training
+    per_device_eval_batch_size=64,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=10,
+)
+
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=val_dataset             # evaluation dataset
+)
+
+trainer.train()
+
+
+#finetuning with pytorch
+
+from torch.utils.data import DataLoader
+from transformers import DistilBertForSequenceClassification, AdamW
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+#model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
+model.to(device)
+model.train()
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+optim = AdamW(model.parameters(), lr=5e-5)
+
+for epoch in range(3):
+    for batch in train_loader:
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+model.eval()
